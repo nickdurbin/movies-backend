7c82d282b6bda50abd55b533659a311e7f5794be
Donald Whitely <dswhitely1@gmail.com>
2019-12-12 16:44:08 -0500

    Final Commit

--- index.js
+++ index.js
@@ -1,23 +1,7 @@
 //import any dependencies
-const server = require("./server.js");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-
 //set up environmental config
 ////setting up environmental variables////
-dotenv.config({ path: "./config/config.env" });
-
-if (process.env.NODE_ENV === "development") {
-  server.use(morgan("dev"));
-}
 
 //define environmental variables if needed
-const PORT = process.env.PORT || 4000;
 
 //set up our server.listen
-const app = server.listen(
-  PORT,
-  console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
-  )
-);
