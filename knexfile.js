cd39cbe92d0f1a866f7588001030e16079bfa0a7
Donald Whitely <dswhitely1@gmail.com>
2019-12-12 11:45:05 -0500

    Knex Config Defined

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/movies.db3'
+    },
+    useNullAsDefault: true,
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys = ON', done);
+      }
+    }
+  },
+
+};
